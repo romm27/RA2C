f5b5aa20d8abbae7d81329c9ab4bbea815b87304
Rommel <giostrasser@gmail.com>
2024-06-05 14:09:13 -0300

    Implements car struct

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "math.h"
 
+typedef struct{
+    char brand[20];
+    char model[20];
+    int manufactureYear;
+    int mileage;
+    int price;
+} Car;
+
 int main(int argc, char const *argv[])
 {
     return 0;
