9d52a6f5ee23276d99b3c50c39222bcb7f39adef
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-06-17 20:39:35 -0300

    Implementa funções de impressão e insersão de novo veículo

--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define size_t ELEMENT_SIZE = sizeof(Car);
+#define MAX_NAME 20
+
 typedef struct{
-    char brand[20];
-    char model[20];
+    char brand[MAX_NAME];
+    char model[MAX_NAME];
     int manufactureYear;
     int mileage;
     int price;
-    struct Car* next;
+    struct Car* next; // ainda não usei a lista encadeada
 } Car;
-
-// #define size_t ELEMENT_SIZE = sizeof(Car);
-#define MAX_CARS 20
-#define MAX_NAME 20
 
 #define OPTIONS    "\n +---------------------------------------------------------------+ \
                     \n |                              MENU                             | \
@@ -23,21 +22,32 @@ typedef struct{
                     \n | [ 1 ] Lista de todos os veículos                              | \
                     \n | [ 2 ] Lista de carros de uma marca                            | \
                     \n | [ 3 ] Lista de carros em um intervalo de valores              | \
-                    \n | [ 4 ] Inserir um novo registro                                | \
+                    \n | [ 4 ] Inserir um novo veículo                                 | \
                     \n | [ 5 ] Remover veículos acima de uma determinada quilometragem | \
                     \n | [ 6 ] Sair                                                    | \
                     \n +---------------------------------------------------------------+ \n"
 
+void print_car(Car c) {
+    printf("Marca:                %s \n");
+    printf("Modelo:               %s \n");
+    printf("Ano de fabricação:    %d \n");
+    printf("Quilometragem:        %d km \n");
+    printf("Preço:                R$ %d,00 \n");
+    printf("\n\n", c.brand, c.model, c.manufactureYear, c.mileage, c.price);
+}
+
+//função que lê do arquivo e transforma em struct
+
+
+//Lê do arquivo e transforma em structs e já imprime
+// O correto é ter a função que lê do arquivo e monta as structs, outra que imprima todas as structs sem ler do arquivo de novo.
 int print_cars_list() {
     FILE *data = fopen("dados.txt", "r");
-    if (data == NULL) {
-        perror("ERRO");
-        exit(1);
-    }
+    if (data == NULL) { perror("ERRO"); exit(1); }
 
-    Car cars[MAX_CARS];
+    Car cars[MAX_NAME];
     char c; // usado para descartar caracteres da entrada
-    int quantity;
+    int quantity; // Estou usando um número que é a primeira linha do arquivo por enquanto, mas deve se contar as linhas e dividir pelo número de linhas de cada veículo para contar quantos tem
 
     fscanf(data, "%d", &quantity);
     // descarta tudo até o LF:
@@ -79,8 +89,46 @@ int print_cars_list() {
     return 0;
 }
 
-int new_car() {
-    // //Cria um novo carro
+// Essa está funcionando certinho
+int insert_new_car() {
+    printf("\nInsira os dados de um novo veículo.\n");
+
+    Car new_car;
+
+    strcpy(new_car.brand, "");
+    strcpy(new_car.model, "");
+
+    getchar();
+    
+    printf("Marca do veículo: ");
+    fgets(new_car.brand, MAX_NAME, stdin);
+    new_car.brand[strcspn(new_car.brand, "\n")] = '\0';
+
+    printf("Modelo do veículo: ");
+    fgets(new_car.model, MAX_NAME, stdin);
+    new_car.model[strcspn(new_car.model, "\n")] = '\0'; 
+
+    printf("Ano de fabricação: ");
+    scanf("%d", &new_car.manufactureYear);
+    getchar();
+
+    printf("Quilometragem: ");
+    scanf("%d", &new_car.mileage);
+    getchar();
+
+    printf("Valor: ");
+    scanf("%d", &new_car.price);
+    getchar();
+
+    puts("\nNovo veículo adicionado com sucesso!");
+
+    // printf("\nMarca: %s\nModelo: %s\nAno de Fabricação: %d\nQuilometragem: %d\nPreço: %d\n",
+    //        new_car.brand, new_car.model, new_car.manufactureYear, new_car.mileage, new_car.price);
+
+    return 0;
+
+
+    // Cria um novo carro
     // Car* p = (Car*) malloc(ELEMENT_SIZE);
     // strcpy(p->brand, "Volkswagen");
     // strcpy(p->model = "Amarok");
@@ -116,6 +164,7 @@ int main(int argc, char const *argv[])
         case 3:
             break;
         case 4:
+            insert_new_car();
             break;
         case 5:
             break;
@@ -128,8 +177,6 @@ int main(int argc, char const *argv[])
             printf("%s", "\nOpção desconhecida. Por favor, escolha uma opção de 1 a 6.\n");
             continue;
         }
-
-
     }
     
     return 0;
